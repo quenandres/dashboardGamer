482729926c89da08744d47b3a57331a471bd9727
Jhonatan Mejia portatil <jhonatan@plasticolab.com>
2023-08-23 13:08:49 -0500

    Add: Sidebar responsive y funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { RiHome3Line, RiFirefoxFill, RiLayoutGridLine, RiUserSettingsLine, RiLiveLine } from "react-icons/ri";
+import { useState } from "react";
+import { 
+  RiHome3Line, 
+  RiFirefoxFill, 
+  RiLayoutGridLine, 
+  RiUserSettingsLine, 
+  RiLiveLine, 
+  RiSettings2Line, 
+  RiMailDownloadLine,
+  RiCheckboxBlankCircleFill,
+  RiMenu5Line,
+  RiCloseFill
+} from "react-icons/ri";
 
 export default function App() {
+  const[showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+
   return (
     <div className='bg-[#141414] min-h-screen'>
-      <siderbar className='bg-[#141414] fixed left-0 top-0 w-80 h-full  border-r border-gray-800 p-8'>
+      <sidebar className={`bg-[#141414] fixed top-0 w-80 h-full  border-r border-gray-800 p-8 flex flex-col justify-between transition-all lg:left-0 ${showMenu ? "left-0" : "-left-full"}`}>
         <div>
           {/* Logo */}
           <h1 className="text-gray-300 uppercase font-bold text-2-xl tracking-[5px] mb-10">Logo</h1>
@@ -40,7 +59,36 @@ export default function App() {
             </li>
           </ul>
         </div>
-      </siderbar>
-    </div>
+        <div>
+        <ul>
+          <li>
+            <a href="#" className="text-gray-300 flex items-center gap-4 hover:bg-[#232323] py-3 px-4 rounded-xl transition-colors" >
+              <RiMailDownloadLine />
+              Downloads
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-300 flex items-center gap-4 hover:bg-[#232323] py-3 px-4 rounded-xl transition-colors" >
+              <RiSettings2Line />
+              Settings
+            </a>
+          </li>
+          <li>
+            <a href="#" className="relative text-gray-300 flex items-center gap-4 hover:bg-[#232323] py-3 px-4 rounded-xl transition-colors" >
+              <img src="https://img.freepik.com/fotos-premium/photo-of-character-cartoon-anime_963769-1805.jpg" className="w-6 h-6 object-cover rounded-full"/>
+              <RiCheckboxBlankCircleFill className="absolute text-green-600 bottom-2 left-8 text-[8px]"/>
+              Tron Legacy
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* Boton movil */}
+      <button 
+      onClick={toggleMenu}
+      className="lg:hidden bg-blue-600 text-white fixed bottom-4 right-6 p-2 text-lg rounded-full">
+        { showMenu ? <RiCloseFill /> : <RiMenu5Line /> }
+      </button>
+    </sidebar>
+  </div>
   )
 }

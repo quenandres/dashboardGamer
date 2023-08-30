4f4cc251e4e54d473db1a29e2129fc03548425a7
Jhonatan Mejia <quenandres@gmail.com>
2023-08-30 18:44:22 -0500

    Cuarta parte

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ https://www.youtube.com/watch?v=KTZT56hgG9U&ab_channel=jotredev
 - 1. Primer parte 29:23min [22/08/2023]
 - 2. Segunda parte 1:02:13min [23/08/2023]
 - 3. Tercera parte 1:30:50min [29/08/2023]
+- 4. Cuarta parte 1:57:55min [30/08/2023]
--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ export default function App() {
         { showMenu ? <RiCloseFill /> : <RiMenu5Line /> }
       </button>
     </sidebar>
-    <header className="fixed lg:pl-[340px] w-full flex flex-col md:flex-row items-center lg:justify-between gap-4 p-8 text-white">
+    <header className="fixed bg-[#242424] lg:pl-[340px] w-full flex flex-col md:flex-row items-center lg:justify-between gap-4 p-8 text-white z-40">
       <nav className="flex items-center gap-4 order-1 lg:order-none">
           <a href="#" className="bg-blue-600 text-white py-2 px-4 rounded-full">Discover</a>
           <a href="#" className="text-gray-500 py-2 px-4 rounded-full">Browse</a>
@@ -121,10 +121,10 @@ export default function App() {
 
     {/* Content */}
     <main className="lg:pl-[340px] p-8 pt-36 ">
-      <div className="grid lg:grid-cols-6">
-        <div className="col-span-5 relative">
+      <div className="grid grid-cols-1 md:grid-cols-6 gap-2">
+        <div className="md:col-span-5 relative">
           <img 
-          className="w-full h-[500px] object-cover"
+          className="w-full h-[500px] object-cover lg:rounded-tr-xl lg:rounded-br-xl"
           src="https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
           <h1 className="absolute top-4 left-4 text-3xl text-white font-bold">League of legends</h1>
           <div className="absolute left-0 bottom-0 p-8 max-w-xl">
@@ -136,7 +136,76 @@ export default function App() {
             </button>
           </div>
         </div>
-        <div className="col-span-1 bg-green-300">Hola2</div>
+
+        {/* Games */}
+        <div className="col-span-1 h-[500px] bg-[#232323] lg:rounded-tr-xl lg:rounded-br-xl w-full">
+          <ul className="p-4 flex flex-col gap-6 h-full overflow-y-scroll">
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images2.alphacoders.com/131/thumbbig-1319576.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Sett</span>
+              </a>
+            </li>          
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images.alphacoders.com/681/681587.png" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Zed</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images4.alphacoders.com/132/thumbbig-1327233.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Gwen</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images2.alphacoders.com/132/thumbbig-1327240.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Nautilus</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images2.alphacoders.com/131/thumbbig-1319576.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Sett</span>
+              </a>
+            </li>          
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images.alphacoders.com/681/681587.png" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Zed</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images4.alphacoders.com/132/thumbbig-1327233.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Gwen</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-4 text-white text-lg md:flex-col lg:flex-row">
+                <img src="https://images2.alphacoders.com/132/thumbbig-1327240.webp" 
+                className="w-20 h-full object-cover"
+                />{" "}
+                <span>Nautilus</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   </div>
